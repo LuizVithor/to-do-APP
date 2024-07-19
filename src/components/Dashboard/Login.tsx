ccb735f6f9d1b6adc4aa03cb8f2b1c6f6298d49d
usrlab22ng <usrlab22ng@academico1.eniac>
2024-07-18 22:19:23 -0300

    Aula de estilização

--- src/components/Dashboard/Login.tsx
+++ src/components/Dashboard/Login.tsx
@@ -1,29 +1,109 @@
-import { Grid, Paper } from "@mui/material";
+import { Button, Grid, Paper, TextField } from "@mui/material";
+import React from "react";
+
+interface childrenI {
+    children: React.ReactNode
+}
+
+const Container = ({ children }: childrenI) => (
+    <Grid
+        container
+        width={"100%"}
+        height={"100%"}
+        alignItems={"center"}
+        justifyContent={"center"}
+    >
+        <Grid
+            sm={5}
+            md={5}
+            lg={5}
+            xs={10}
+            container
+            height={"70%"}
+        >
+            <Paper
+                elevation={20}
+                style={{
+                    width: "100%",
+                    borderRadius: "20px"
+                }}
+            >
+                {children}
+            </Paper>
+        </Grid>
+    </Grid>
+)
 
 export const Login = () => {
     return (
-        <Grid
-            container
-            width={"100%"}
-            height={"100%"}
-            alignItems={"center"}
-            justifyContent={"center"}
-        >
+        <Container>
             <Grid
-                xs
-                sm={6}
-                md={6}
-                lg={6}
                 container
-                width={"100%"}
-                height={"700px"}
+                height={"100%"}
+                padding={"0px 20px"}
+                alignItems={"center"}
+                flexDirection={"column"}
+                justifyContent={"space-evenly"}
             >
-                <Paper
-                    elevation={20}
-                >  
-
-                </Paper>
+                <Grid
+                    item
+                    sx={{
+                        width: "100%"
+                    }}
+                >
+                    <TextField
+                        fullWidth
+                        size={"small"}
+                        label={"Email ou Nome de usuário"}
+                    />
+                </Grid>
+                <Grid
+                    item
+                    sx={{
+                        width: "100%"
+                    }}
+                >
+                    <TextField
+                        fullWidth
+                        size={"small"}
+                        label={"Senha"}
+                    />
+                </Grid>
+                <Grid
+                    container
+                    spacing={2}
+                    justifyContent={"center"}
+                >
+                    <Grid
+                        lg={4}
+                        sm={4}
+                        xs={4}
+                        md={4}
+                        item
+                    >
+                        <Button
+                            fullWidth
+                            variant="outlined"
+                        >
+                            Registrar
+                        </Button>
+                    </Grid>
+                    <Grid
+                        lg={4}
+                        sm={4}
+                        xs={4}
+                        md={4}
+                        item
+                    >
+                        <Button
+                            fullWidth
+                            variant="contained"
+                        >
+                            Entrar
+                        </Button>
+                    </Grid>
+                </Grid>
             </Grid>
-        </Grid>
+        </Container>
     )
 };
